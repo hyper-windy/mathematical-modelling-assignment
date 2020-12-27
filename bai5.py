5f7ff9fe121bba09324ab2f91e49f7d3b1a0d8d6
Black-Frost <vovantiendung2001@gmail.com>
2020-12-27 20:49:35 +0700

    ex5

--- bai5.py
+++ bai5.py
@@ -2,6 +2,12 @@ def cap_VP_air(h_air, T_air):
     M_water = 
     R = 
     return (M_water * h_air)/(R * T_air + 273.15)
+
+def MV_8_43(VP1, VP2, HEC):
+    if (VP1 < VP2):
+        return 0
+    else:
+        return (6.4 * (10.0 ** -9)) * HEC * (VP1 - VP2)
     
 def MV_can_air():
     
