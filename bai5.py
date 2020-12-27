b61635410d1ccd5a6da625f9395651abc15b1e2b
Nguyễn Tấn Đạt <55391896+Jinnux263@users.noreply.github.com>
2020-12-27 20:08:44 +0700

    Create bai5.py

---
+++ bai5.py
@@ -0,0 +1,22 @@
+def MV_can_air():
+    
+def MV_pad_air():
+
+def MV_fog_air():
+
+def MV_blow_air():
+
+def MV_air_thscr():
+
+def MV_air_top():
+
+def MV_air_out():
+
+def MV_airout_pad():
+
+def MV_air_mech():
+
+def MV_top_out():
+
+def MV_top_covin():
+
