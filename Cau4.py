91bb59782d6d8c69c2725651008d66fcda096b9b
Nguyễn Tấn Đạt <55391896+Jinnux263@users.noreply.github.com>
2020-12-27 17:13:35 +0700

    Update Cau4.py

--- Cau4.py
+++ Cau4.py
@@ -10,30 +10,8 @@ def rk4(dx, x0, y0, h):
     return y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4) 
 
 ####################
-def func( x, y ): 
-    return (x + y + x * y) 
-      
-# Function for euler formula 
-def euler( x0, y, h, x ): 
-    temp = -0
+def euler(dx, x0, y, h): 
+
+    y = y + h * dx(x0, y) 
   
-    # Iterating till the point at which we 
-    # need approximation 
-    while x0 < x: 
-        temp = y 
-        y = y + h * func(x0, y) 
-        x0 = x0 + h 
-  
-    # Printing approximation 
-    print("Approximate solution at x = ", x, " is ", "%.6f"% y) 
-      
-# Driver Code 
-# Initial Values 
-x0 = 0
-y0 = 1
-h = 0.025
-  
-# Value of x at which we need approximation 
-x = 0.1
-  
-euler(x0, y0, h, x) 
+    return y
