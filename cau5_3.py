ea092c9b72337d72dc2fd479a25162a318d95cc3
Nguyễn Tấn Đạt <55391896+Jinnux263@users.noreply.github.com>
2021-01-08 23:12:35 +0700

    Update cau5_3.py

--- cau5_3.py
+++ cau5_3.py
@@ -8,30 +8,50 @@ def cal_VP(rhAir, t):
     return rhAir * cal_saturation_pressure(t) / 100.0
       
 data = []
-with open("data\\Greenhouse_climate.csv", "r") as f:
+with open("data//Greenhouse_climate.csv", "r") as f:
     csv_file = csv.DictReader(f)
     for row in csv_file:
         data.append(row)
 
-solver = Solver()
-#determine the start of the dataset
-start = 0
-startData = "NaN"
-while startData == "NaN":
-    startData = data[start]["CO2air"]
-    start += 1
-start -= 1
+vip = []
+with open("data\\vip.csv", "r") as f:
+    csv_file = csv.DictReader(f)
+    for row in csv_file:
+        vip.append(row)
 
-for i in range(start,300):
-    VP_air = cal_VP(float(data[i]["RHair"]), float(data[i]["Tair"]))
-    T_air = float(data[i]["Tair"])  #air temperature
-    T_out = T_air + 1               #temperature of the air outside
-    T_thscr = T_air + 1             #temperature of the thermal screen
-    T_top = T_air                   #temperature in the top room
-    VP_out = VP_air
+climate = []
+with open("data\\Greenhouse_climate.csv", "r") as f:
+    csv_file = csv.DictReader(f)
+    for row in csv_file:
+        climate.append(row)
+
+meteo = []
+with open("data//meteo.csv", "r") as k:
+    csv_file = csv.DictReader(k)
+    for row in csv_file:
+        meteo.append(row)
+
+solver = Solver()
+
+for i in range(100):
+    VP_air = cal_VP(float(climate[i]["RHair"]), float(climate[i]["Tair"]))
     VP_top = VP_air
+
+    T_air = float(climate[i]["Tair"])  # air temperature
+    T_out = float(meteo[i]["Tout"])  # temperature of the air outside
+    T_thscr = T_air + 1     # temperature of the thermal screen
+    T_top = T_air  # temperature in the top room
+    T_can = T_air + 1
+    T_covin = T_air
+    v_wind = float(meteo[i]["Windsp"])
+
+    VP_can = cal_saturation_pressure(T_can)
+    VP_out = cal_VP(float(meteo[i]["Rhout"]), float(meteo[i]["Tout"]))
     VP_thscr = cal_saturation_pressure(T_thscr)
-    U_roof = (float(data[i]["VentLee"]) + float(data[i]["Ventwind"])) / 2 / 100.0
-    U_thscr = float(data[i]["EnScr"]) / 100
-    d = solver.dx(VP_air = VP_air, T_air = T_air, VP_out = VP_out, T_out = T_out, T_top = T_top, T_thscr = T_thscr, U_roof = U_roof, U_thscr = U_thscr, VP_top = VP_top, VP_thscr = VP_thscr)
+
+    U_roof = (float(climate[i]["VentLee"]) + float(climate[i]["Ventwind"])) / 2 / 100.0
+    U_thscr = float(climate[i]["EnScr"]) / 100
+    # print(str(i) + ": "),
+    
+    d = solver.dx(VP_air=VP_air, T_air=T_air, VP_out=VP_out, T_out=T_out, T_top=T_top, VP_top=VP_top,T_thscr=T_thscr, U_roof=U_roof, U_thscr=U_thscr, VP_thscr=VP_thscr, VP_can=VP_can, v_wind=v_wind,T_covin=T_covin)
     print("VP_air' = %f \t\t VP_top' = %f" %(d[0], d[1]))
